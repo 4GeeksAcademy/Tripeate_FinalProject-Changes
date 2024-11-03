7f8cfe7eb5de23e43a53f95e2847b1693d579cd9
Marian <marianvsf@gmail.com>
2024-11-03 03:12:01 +0000

    route logout

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, UserAdmin, Plan, Role, Beach, Mountain, City
+from .models import db, User, UserAdmin, Plan, Role, Beach, Mountain, City, TokenBlockedList
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -18,6 +18,7 @@ def setup_admin(app):
     admin.add_view(ModelView(Beach, db.session))
     admin.add_view(ModelView(Mountain, db.session))
     admin.add_view(ModelView(City, db.session))
+    admin.add_view(ModelView(TokenBlockedList, db.session))
 
 
     # You can duplicate that line to add mew models
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt, get_jwt_identity
 from flask_bcrypt import Bcrypt
-from api.models import db, User
+from api.models import db, User, TokenBlockedList
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -65,7 +65,15 @@ def user_login():
 def protected():
     current_user_id = get_jwt_identity()
     user = User.query.get(current_user_id)
-    return jsonify(user.serialize()), 200
+    return jsonify({"msg":"Perfil del usuario"}, user.serialize()), 200
 
+@api.route('/logout', methods=["POST"])
+@jwt_required()
+def user_logout():
+    token_data = get_jwt()
+    token_blocked = TokenBlockedList(jti = token_data["jti"])
+    db.session.add(token_blocked)
+    db.session.commit()
+    return jsonify({"msg":"Sesión cerrada"}) 
 
  
