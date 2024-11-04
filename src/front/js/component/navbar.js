453a5ad86f9177d2e67efbf6411f90c50099dc8b
Souwg <sansanvzla@gmail.com>
2024-11-04 02:06:19 +0000

    creacion del reistro de usuario

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div className="container-fluid" style={{ background: "#ddd" }}>
+      <div className="container">
+        <nav className="navbar bg-body-secondary">
+          <Link to="/">
+            <div className="navbar-brand mb-0">
+              <h2>Tripeate</h2>
+            </div>
+          </Link>
+          <div className="ml-auto">
+            <Link to="/register">
+              <button className="btn btn-primary">Regístrate</button>
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 };
