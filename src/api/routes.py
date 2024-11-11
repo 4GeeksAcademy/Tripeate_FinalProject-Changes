18a173f699e0104fe6c259ffb5160542e6650c87
Marian <marianvsf@gmail.com>
2024-11-11 17:54:30 +0000

    login funciona

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt, get_jwt_identity, current_user
 from flask_bcrypt import Bcrypt
-from api.models import db, User, Plan, PlanType, TokenBlockedList
+from api.models import db, User, Plan, TokenBlockedList
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -28,25 +28,28 @@ def handle_hello():
 def signup_user():
     # Se reciben los datos de la petición
     body=request.get_json()
-    # Se valida que se esta ingresando un usuario y contraseña
+    if body['name'] is None:
+        return jsonify({"msg":"El campo nombre y apellido es obligatorio"})
     if body['email'] is None:
-        return jsonify({"msg":"Por especifique un nombre de usuario"}),400
+        return jsonify({"msg":"Por especifique un correo electrónico"}),400
     if body['password'] is None:
         return jsonify({"msg":"Por favor especifique su contraseña"}),400
     # Se encripta la contraseña
     body['password']=bcrypt.generate_password_hash(body['password']).decode('utf-8')
     # Se guarda en la base de datos 
-    user = User(email=body['email'], password=body['password'], is_active=True)
+    user = User(name=body['name'], last_name=body['last_name'], email=body['email'], password=body['password'], is_active=True)
     db.session.add(user)
     db.session.commit()
     return jsonify({"msg":"Usuario creado con exito", "user": user.serialize()})
+    
 
 # Ruta para formulario de inicio de sesión
 @api.route('/login', methods=['POST'])
 def user_login():
+    new_user = body
     body = request.get_json()
     if body["email"] is None:
-        return jsonify({"msg":"Debe especificar una contraseña"}), 400
+        return jsonify({"msg":"Debe especificar un correo electrónico"}), 400
     # Se busca el usuario en la base de datos y se verifica que exista
     user = User.query.filter_by(email=body["email"]).first()
     if user is None:
@@ -54,7 +57,7 @@ def user_login():
     # Se comparar la contraseña proporcionada por el usuario con un hash de contraseña almacenado previamente
     valid_password = bcrypt.check_password_hash(user.password, body["password"])
     if not valid_password:
-        return jsonify({"msg": "Clave invalida"}), 401
+        return jsonify({"msg": "Contraseña incorrecta"}), 401
     # Se crea y se retorna el token de la sesión
     token = create_access_token(identity=user.id, additional_claims={"role": "admin"})
     return jsonify({"msg": "Login exitoso", "token": token, "Id": user.id })
