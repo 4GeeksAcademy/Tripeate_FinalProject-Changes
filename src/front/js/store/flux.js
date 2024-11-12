17a0cd078310e9a4cd8252630f0fb573060a06f1
Souwg <sansanvzla@gmail.com>
2024-11-12 02:18:18 +0000

    integracion del backend con el frontend del regiteruser

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,31 +1,38 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			user: null,
-			
+
 		},
 		actions: {
-
-			registerUser: async (userData) => {
+			signupUser: async (nombre, apellido, email, password) => {
 				try {
-				  const response = await fetch("", {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/json"
-					},
-					body: JSON.stringify(userData)
-				  });
-				  if (!response.ok) throw new Error("Error al registrar usuario");	
-				  const data = await response.json();
-				  console.log("Usuario registrado con éxito:", data);
-				  // Actualiza el estado del usuario en el store
-				  setStore({ user: data });
-				  return data;
+					const response = await fetch(`https://hidden-spider-5g49j5g447grc45x7-3001.app.github.dev/api/signup`, {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							nombre: nombre,
+							apellido: apellido,
+							email: email,
+							password: password,
+						}),
+					});
+
+					if (response.ok) {
+						const data = await response.json();
+						console.log("Usuario registrado:", data.user);
+						return data; // Devuelve los datos si el registro fue exitoso
+					} else {
+						const errorData = await response.json();
+						console.error("Error en el registro:", errorData.msg);
+						return { error: true, msg: errorData.msg }; // Devuelve un mensaje de error si falló
+					}
 				} catch (error) {
-				  console.error("Error en el registro:", error);
-				  return null;
+					console.error("Error en la solicitud:", error);
+					return { error: true, msg: "Error en la solicitud" }; // Indica un error de red u otro tipo de error
 				}
-			  },
+			},
 		}
 	};
 };
