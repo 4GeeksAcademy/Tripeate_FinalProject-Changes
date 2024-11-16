a935d6dc2320b6d428011f88dde8c0efb2bea1aa
Marian <marianvsf@gmail.com>
2024-11-16 04:53:47 +0000

    estilos del modalLogin

--- src/front/js/component/modalError.js
+++ src/front/js/component/modalError.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Modal from 'react-modal';
+import "../../styles/modalError.css";
 
 const ErrorModal = ({ isOpen, onRequestClose, errorMessage }) => {
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose} ariaHideApp={false}>
-            <h5 className="modal-title">Error</h5>
-            <p>{errorMessage}</p>
-            <button onClick={onRequestClose}>Cerrar</button>
+        <Modal 
+            isOpen={isOpen} onRequestClose={onRequestClose} ariaHideApp={false}
+            className="modal-content"  overlayClassName="modal-overlay" 
+        >
+            <div className="modal-header">
+                <h5 className="modal-title">Se ha producido un error</h5>
+                <button type="button" className="btn-close" onClick={onRequestClose}aria-label="Close"
+                ></button>
+            </div>
+            <div className="modal-body">
+                <h6>{errorMessage}</h6>
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-primary" onClick={onRequestClose}
+                >
+                    Cerrar
+                </button>
+            </div>
         </Modal>
     );
 };
