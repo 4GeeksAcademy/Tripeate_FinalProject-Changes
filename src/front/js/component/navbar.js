c1291e86a169b89e54c53e06eabd98a723ae893c
Marian <marianvsf@gmail.com>
2024-12-16 15:42:02 +0000

    boton navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogOut = async () => {
     const result = await actions.logoutUser();
@@ -34,19 +35,30 @@ export const Navbar = () => {
             </div>
           </Link>
           <div className="ml-auto">
-            {store.token ? ( // Si hay token, mostrar "Cerrar Sesión"
-              <button
-                className="btn btn-outline-danger ml-2"
-                onClick={()=>handleLogOut()}
-              >
-                Cerrar Sesión
-              </button>
-            ) : ( // Si no hay token, mostrar "Iniciar Sesión"
+            {store.token ? ( // Si hay token, mostrar "Cerrar Sesión" y el botón del perfil
+            <>
+            {location.pathname !== "/userinfo" && (
+              <Link to="/userinfo">
+                <button className="btn btn-light me-2" style={{ fontWeight: "bold" }}>
+                  Mi Perfil
+                </button>
+              </Link>
+              )}
+                <button
+                  className="btn btn-outline-light ml-2"
+                  onClick={()=>handleLogOut()}
+                  >
+                  Cerrar Sesión
+                </button>
+            </>
+            ) : ( // Si no hay token y no estamos en la página de login, mostrar "Iniciar Sesión"
+              location.pathname !== "/loginuser" && (
               <Link to="/loginuser">
                 <button className="btn btn-light" style={{ fontWeight: "bold" }}>
                   Iniciar Sesión
                 </button>
               </Link>
+              )
             )}
           </div>
         </nav>
